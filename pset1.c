3bcff9fa47708a375487c48577affceeeb889be2
tonywp955 <31873993+tonywp955@users.noreply.github.com>
2017-09-12 12:13:06 -0400

    Update pset1.c

--- pset1.c
+++ pset1.c
@@ -5,71 +5,76 @@ int main(int argc, char *argv[])
 {
     FILE *p;
     char ch;
-    char cho = '\0';
 
     p = fopen(argv[1], "r");
 
-    const int raw = 1;
-    const int startpot = 2;
-    const int cmt1 = 3;
-    const int cmt2 = 4;
-    const int endpot = 5;
-    const int blackhole1 = 6;
-    const int blackhole2 = 7;
-
+    enum STATE {
+        raw = 1,
+        startpot = 2,
+        cmt1 = 3,
+        cmt2 = 4,
+        endpot = 5,
+        blackhole1 = 6,
+        blackhole2 = 7,
+        quote1 = 8,
+        quote2 = 9
+    };
+    
     bool block = true;
-    int state = raw;
+    bool quote = true;
+    enum STATE state = raw;
     
     while((ch = fgetc(p)) != EOF)
     {
-        switch(state) {
-            case raw:
+        if(state == raw){
                 if (ch == '/') {
                     state = startpot;
                 }
-                break;
-            case startpot:
-                if (ch == '/') {
+        }else if(state == startpot){
+
+            if (ch == '/') {
                     state = cmt1;
                     block = true;
                 } else if (ch == '*') {
                     state = cmt2;
-                    
                 } else {
                     state = raw;
                 }
-                break;
-            case cmt1:
-                if (ch == '\n') {
+        }         else if (state == cmt1) {
+            if (ch == '\n') {
                     printf("\n");
                     state = raw;
                 } else if (ch == '@') {
                     state = blackhole1;
                     block = true;
                 } else if (ch == ' ' && block == true){}
+                else if (ch == '"') {
+                    state = quote1;
+                }
                 else {
                     printf("%c", ch); 
                     block = false;
                 }
-                break;
-            case cmt2:
-                if (ch == '*') {
+                
+        }else if (state == cmt2)                
+        {if (ch == '*') {
                     state = endpot;
                 } else if (ch == '@') {
                     block = true;
                     state = blackhole2;
-                } else if (ch == '\n'){
+                } else if (ch == '\n' && quote == true) {
                     block = true;
                     printf("\n");
                 }else if (ch == ' ' && block == true) {
-                    //printf("what");
-                }else{
+                }else if (ch == '"') {
+                    state = quote2;
+                 }
+        else{ if (quote == true) {
                     printf("%c", ch);
                     block = false;
+        }
                 }
-
-                break;
-            case endpot:
+        }else if (state == endpot) {
                 if (ch == '/') {
                     block=false;
                     state = raw;
@@ -81,28 +86,29 @@ int main(int argc, char *argv[])
                     printf("\n");
                     block = true;
                 }else if (ch==' ' && block == true){
-                }
+                    state = cmt2;
+                } else if (ch == '"') {
+                    state = quote2;
+                    block = false;}
                 else {
                     printf("%c", ch);
                     block = false;
                     state = cmt2;
                 }
-                break;
-            case blackhole1:
+        } else if (state == blackhole1) {
                 if (ch == ' ')
                 {
                     state = cmt1;
                 }
-                break;
-            case blackhole2:
-                if (ch == ' ')
+        } else if (state == blackhole2)
+        { if (ch == ' ')
                 {
                     state = cmt2;
                 }
-            default:
-                break;
-        }
-        cho = ch;
+        } else if (state == quote1) {
+            if (ch == '"') {state = cmt1; quote = false;}         
+        } else if (state == quote2) {
+            if (ch == '"') {state = cmt2; quote = false;}         
+        }            
     }
 }
-
